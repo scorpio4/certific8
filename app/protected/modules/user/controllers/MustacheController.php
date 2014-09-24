87b4aaee9b7e4bb58f8b5ffeb69c8c1125437074
joji <joji@zyntegra.com>
2014-09-24 12:46:53 +0530

    mustache added

---
+++ app/protected/modules/user/controllers/MustacheController.php
@@ -0,0 +1,44 @@
+<?php
+
+class MustacheController extends Controller
+{
+	public function actionIndex()
+	{
+            spl_autoload_unregister(array('YiiBase','autoload'));
+            Yii::import('application.vendor.mustache.*');
+            require_once('src/Mustache/Autoloader.php');
+            Mustache_Autoloader::register();
+            
+            $m = new Mustache_Engine();
+            echo $m->render('Hello, {{planet}}!', array('planet' => 'World')); // "Hello,
+            
+            spl_autoload_register(array('YiiBase', 'autoload'));
+	}
+
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
---
+++ app/protected/modules/user/views/mustache/index.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this MustacheController */
+
+$this->breadcrumbs=array(
+	'Mustache',
+);
+?>
+<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+
+<p>
+	You may change the content of this page by modifying
+	the file <tt><?php echo __FILE__; ?></tt>.
+</p>
